86de6c1ea6e6b15cfa74c52328cff55ca0f12fab
John Schieferle Uhlenbrock <johnsu@provisioinsights.com>
2025-04-18 17:31:29 -0500

    Add Tailwind CSS setup for documentation site
    - Created package.json to manage project dependencies and build scripts for Tailwind CSS.
    - Added postcss.config.js to configure PostCSS with Tailwind CSS and Autoprefixer.
    - Initialized tailwind.config.js to define content sources and extend theme colors for the documentation site.

---
+++ docs/tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    content: [
+      "./_layouts/**/*.html",
+      "./**/*.md",
+      "./visualizer/**/*.html"
+    ],
+    theme: {
+      extend: {
+        colors: {
+          navy:    "#0A1F35",
+          teal:    "#00CFB0",
+          skyblue: "#009EEB",
+        }
+      }
+    },
+    plugins: []
+  };
+  
